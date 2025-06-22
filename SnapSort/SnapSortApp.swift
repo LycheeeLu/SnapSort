fb58f8f61c7bcde53fa47381e92877ff74d18948
lujie <jie.lu@tuni.fi>
2025-06-22 20:39:21 +0800

    Initial Commit

---
+++ SnapSort/SnapSortApp.swift
@@ -0,0 +1,32 @@
+//
+//  SnapSortApp.swift
+//  SnapSort
+//
+//  Created by Jie Lu on 22.6.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SnapSortApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SnapSortTests/SnapSortTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnapSortTests.swift
+//  SnapSortTests
+//
+//  Created by Jie Lu on 22.6.2025.
+//
+
+import Testing
+@testable import SnapSort
+
+struct SnapSortTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
