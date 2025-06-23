fd8299fe9abd05fd2e6f550f52ca4a1302fa5882
lujie <jie.lu@tuni.fi>
2025-06-23 17:49:10 +0800

    Add: classification service structure

---
+++ SnapSort/Services/ClassificationService.swift
@@ -0,0 +1,41 @@
+//
+//  ClassifyService.swift
+//  SnapSort
+//
+//  Created by Jie Lu on 22.6.2025.
+//
+
+
+import Foundation
+
+class ClassificationService{
+    
+    //main classificaton method
+    static func classifyText() -> [String]{
+        
+        
+        
+    }
+    
+    
+    
+    //calculating relevance score for a topic
+    private static func calculateThemeScore() -> Int{
+        
+    }
+    
+    
+    // get classification confidence (0 to 1.0)
+    static func getConfidence(for text: String, themes: [String], allThemes: [Keyword]) -> Double{
+        
+    }
+    
+    
+    
+    //default topics for new users
+    //will do more complex matching (like per-word tokenization or machine learning) later
+    static func createDefaultKeywords() -> [Keyword]{
+        
+    }
+}
+
--- SnapSort/Services/ClassifyService.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ClassifyService.swift
-//  SnapSort
-//
-//  Created by Jie Lu on 22.6.2025.
-//
-
-
-
