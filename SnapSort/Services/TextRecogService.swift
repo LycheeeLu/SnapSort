d2f32e2bab8e89b6093f0c040e1ffc41d356c049
lujie <jie.lu@tuni.fi>
2025-06-23 17:38:32 +0800

    Add: text recog service batch processing

--- SnapSort/Services/TextRecogService.swift
+++ SnapSort/Services/TextRecogService.swift
@@ -18,6 +18,7 @@ class TextRecogService: ObservableObject {
     //UIImage is the class for processing image in iOS
     //CGImage is data structure in Core Graphics
     //OCR requires turning UIImage into CGImage
+    // this is function for single image OCR
     func extractText(from image: UIImage) async -> String{
         guard let cgImage = image.cgImage else {
             print("Could not get CGImage from UIImage")
@@ -47,10 +48,58 @@ class TextRecogService: ObservableObject {
             }
             
             //configure requests for better accuracy
+            request.recognitionLevel = .accurate
+            request.usesLanguageCorrection = true
+            request.recognitionLanguages = ["en-US", "zh-Hans" ,"ja-JP"]
             
+            let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+            
+            do{
+                try handler.perform([request])
+            } catch{
+                print("Failed to perform text recognition:")
+                print("error: \(error.localizedDescription)")
+                continuation.resume(returning: "")
+            }
         }
         
 
+    }
+    
+    
+    //Batch processing multiple images in chunks of 10
+    // to avoid lag or overloading
+    func extractTextFromImages(_ images: [UIImage]) async ->[String] {
+        isProcessing = true
+        
+        //defer ensures isProcessing is reset when finished, even if early returns or errors happen.
+        defer { isProcessing = false }
+        
+        
+        var results: [String] = []
+        let chunkSize = 10
+        let total = images.count
+        
+        
+        //stride() walk through the list 10 at a time
+        for chunkStart in stride(from: 0, to: total, by: chunkSize){
+            let chunk = Array(images[chunkStart..<min(chunkStart + chunkSize, total)])
+            
+            for image in chunk {
+                let text = await extractText(from: image)
+                results.append(text)
+            }
+        }
+        
+       
+        
+        return results
+        
+    }
+
+        //Helper to clean and normalize extracted text
+    func cleanText( _ text: String)-> String{
+        return text.trimmingCharacters(in: .whitespacesAndNewlines).replacingOccurrences(of: "\\s+", with:  " ", options:.regularExpression)
     }
     
     
